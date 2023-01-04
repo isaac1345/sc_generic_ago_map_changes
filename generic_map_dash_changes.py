8746513dd51cda5e2655f004de1cec698865da8c
isaac1345 <isaac1345@gmail.com>
2023-01-04 10:01:22 -0800

    Fixed deletion logic, added separate usage example

--- generic_map_dash_changes.py
+++ generic_map_dash_changes.py
@@ -6,20 +6,14 @@ This script is set up in sections, with '# %%' delineating sections, and allowin
 This code is set up to be content agnostic; you should be able to copy and paste it into your project and adapt it to work with your data for most common usages
 
 Updates:
+v0.2:
+Deletion is now mutually exlusive with other layer operations
 v0.1
 '''
 
 # %%
 ############ Imports
 from arcgis.gis import GIS
-
-# %% 
-############ log in and set up your AGO connection
-# Depending on your script, you may want to handle passwords differently
-url = 'http://governmentofbc.maps.arcgis.com' # change to your url, whether maphub, geohub, or other
-username = 'foo'
-password = 'bar'
-gis = GIS(url, username, password)
 
 # %% 
 ############ Functions and classes
@@ -43,14 +37,15 @@ class derived_map:
         if layer_level == "initial":
             layer_level=self.prim_wm_json['operationalLayers']
         for i in reversed(range(len(layer_level))):
-            if layer_level[i]['id'] in self.vis_list:
-                layer_level[i]['visibility'] = True
-            else:
-                layer_level[i]['visibility'] = False
-            if layer_level[i]['id'] in self.del_list: # causes indexing issues
+            if layer_level[i]['id'] in self.del_list:
                 del layer_level[i] # the item to del
-            elif layer_level[i]['layerType'] == 'GroupLayer':
-                self.changes(layer_level[i]['layers'])
+            else:
+                if layer_level[i]['id'] in self.vis_list:
+                    layer_level[i]['visibility'] = True
+                else:
+                    layer_level[i]['visibility'] = False
+                if layer_level[i]['layerType'] == 'GroupLayer':
+                    self.changes(layer_level[i]['layers'])
     def push(self): # push changes to the new_id
         self.deriv_wm_item = gis.content.get(self.new_id)
         self.deriv_wm_item.update(data = self.prim_wm_json)
@@ -70,29 +65,3 @@ class derived_dashboard:
         self.deriv_wm_item = gis.content.get(self.new_id)
         self.deriv_wm_item.update(data = self.prim_db_json)
 
-# %% 
-############ Setting lists of changes
-
-map_list = [
-    derived_map(
-        "8aab32af2c4d48b091dadb55592f723b",
-        "33b382a5fda74641a658128f7c3513b4",
-        ["182cc61d735-layer-4"], 
-        ["18517e8ca9d-layer-10"]
-        ) # initial example
-]
-
-dashboard_list = [
-    
-]
-
-# %% 
-############ Running the code
-
-for in_map in map_list:
-    in_map.changes()
-    in_map.push()
-
-for in_db in dashboard_list:
-    in_db.changes()
-    in_db.push()
